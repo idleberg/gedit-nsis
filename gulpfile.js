ca4c5a1d92684d8c1e870ed5998f1c39511f01ef
Jan T. Sott <jan@idleberg.com>
2017-07-26 20:20:56 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Dependencies
+var gulp = require('gulp'),
+    debug = require('gulp-debug'),
+    xmlVal = require('gulp-xml-validator');
+
+// File patterns
+var xmlFiles = [
+    'nsis.lang'
+];
+
+// Validate XML
+gulp.task('lint:xml', gulp.series( (done) => { 
+  gulp.src(xmlFiles)
+    .pipe(debug({title: 'xml-validator'}))
+    .pipe(xmlVal());
+  done();
+}));
+
+// Available tasks
+gulp.task('lint', gulp.parallel('lint:xml', (done) => {
+  done();
+}));
